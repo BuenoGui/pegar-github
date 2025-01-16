e694fe822ed770c55e4b14aa12eac63f1afd975a
Guilherme B <dogui0202@gmail.com>
2025-01-16 19:29:25 -0300

    acho que foi

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    const endpoint = ('https://api.github.com/users/BuenoGui');
+
+    const nome = document.querySelector('#nome');
+    const usuario = document.querySelector('#usuario');
+    const icone = document.querySelector('#icone');
+    const repositorios = document.querySelector('#repositorios');
+    const seguidores = document.querySelector('#seguidores');
+    const seguindo = document.querySelector('#seguindo');
+    const github = document.querySelector('#github');
+
+    fetch(endpoint).then(function(answers) {
+        return answers.json();
+    }).then (function(json) {
+        nome.innerText = json.name;
+        usuario.innerText = json.login;
+        icone.src = json.avatar_url;
+        seguindo.innerText = json.followeing;
+        seguidores.innerText = json.followers;
+        repositorios.innerText = json.public_repos;
+        github.href = json.html_url;
+    })
+    
+
+})
